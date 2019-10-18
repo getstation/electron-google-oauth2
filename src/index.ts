987c1fd4f539c7b6573f89b01e16b4147d8bb8d1
Alexandre Lacheze <alexandre.lacheze@gmail.com>
2019-10-18 16:21:59 +0200

    Use loopback interface redirection (#2)
    * Working state
    
    * Refocus + configurable redirect URI
    
    * ✏️
    
    * 🤣
    
    * Add options

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 // inspired by https://github.com/parro-it/electron-google-oauth
-import { remote, BrowserWindow } from 'electron';
+import { shell, BrowserWindow, remote } from 'electron';
 import { EventEmitter } from 'events';
 import { OAuth2Client } from 'google-auth-library';
 import { Credentials } from 'google-auth-library/build/src/auth/credentials';
 import { google } from 'googleapis';
 import { stringify } from 'querystring';
 import * as url from 'url';
+import LoopbackRedirectServer from './LoopbackRedirectServer';
 
 const BW: typeof BrowserWindow = process.type === 'renderer' ? remote.BrowserWindow : BrowserWindow;
 
@@ -22,6 +23,19 @@ export class UserClosedWindowError extends Error {
  * @type {Credentials}
  */
 
+export type ElectronGoogleOAuth2Options = {
+  successRedirectURL: string,
+  loopbackInterfaceRedirectionPort: number,
+  refocusAfterSuccess: boolean,
+};
+
+export const defaultElectronGoogleOAuth2Options: ElectronGoogleOAuth2Options = {
+  successRedirectURL: 'https://getstation.com/app-login-success/',
+  // can't be randomized
+  loopbackInterfaceRedirectionPort: 42813,
+  refocusAfterSuccess: true,
+};
+
 /**
  * Handle Google Auth processes through Electron.
  * This class automatically renews expired tokens.
@@ -31,24 +45,32 @@ export default class ElectronGoogleOAuth2 extends EventEmitter {
 
   public oauth2Client: OAuth2Client;
   public scopes: string[];
+  protected server: LoopbackRedirectServer | null;
+  protected options: ElectronGoogleOAuth2Options;
 
   /**
    * Create a new instance of ElectronGoogleOAuth2
    * @param {string} clientId - Google Client ID
    * @param {string} clientSecret - Google Client Secret
    * @param {string[]} scopes - Google scopes. 'profile' and 'email' will always be present
-   * @param {string} redirectUri - defaults to 'urn:ietf:wg:oauth:2.0:oob'
+   * @param {string} successRedirectURL
    */
-  constructor(clientId: string, clientSecret: string, scopes: string[], redirectUri: string = 'urn:ietf:wg:oauth:2.0:oob') {
+  constructor(
+    clientId: string,
+    clientSecret: string,
+    scopes: string[],
+    options: Partial<ElectronGoogleOAuth2Options> = defaultElectronGoogleOAuth2Options,
+  ) {
     super();
     // Force fetching id_token if not provided
     if (!scopes.includes('profile')) scopes.push('profile');
     if (!scopes.includes('email')) scopes.push('email');
     this.scopes = scopes;
+    this.options = { ...defaultElectronGoogleOAuth2Options, ...options };
     this.oauth2Client = new google.auth.OAuth2(
       clientId,
       clientSecret,
-      redirectUri
+      `http://127.0.0.1:${this.options.loopbackInterfaceRedirectionPort}/callback`
     );
     this.oauth2Client.on('tokens', (tokens) => {
       this.emit('tokens', tokens);
@@ -64,6 +86,7 @@ export default class ElectronGoogleOAuth2 extends EventEmitter {
     let url = this.oauth2Client.generateAuthUrl({
       access_type: 'offline', // 'online' (default) or 'offline' (gets refresh_token)
       scope: this.scopes,
+      redirect_uri: `http://127.0.0.1:${this.options.loopbackInterfaceRedirectionPort}/callback`
     });
 
     if (forceAddSession) {
@@ -90,51 +113,44 @@ export default class ElectronGoogleOAuth2 extends EventEmitter {
    * @returns {Promise<string>}
    */
   openAuthWindowAndGetAuthorizationCode(urlParam: string) {
-    return new Promise<string>((resolve, reject) => {
-      const win = new BW({
-        useContentSize: true,
-        fullscreen: false,
-      });
-
-      win.loadURL(urlParam);
-
-      win.on('closed', () => {
-        reject(new UserClosedWindowError());
-      });
-
-      function closeWin() {
-        win.removeAllListeners('closed');
-        setImmediate( () => {
-          win.close();
-        });
-      }
-
-      win.webContents.on('did-navigate', (_event, newUrl) => {
-        const parsed = url.parse(newUrl, true);
-        if (parsed.query.error) {
-          reject(new Error(parsed.query.error_description as string));
-          closeWin();
-        } else if (parsed.query.code) {
-          resolve(parsed.query.code as string);
-          closeWin();
-        }
-      });
-
-      win.on('page-title-updated', () => {
-        setImmediate(() => {
-          const title = win.getTitle();
-          if (title.startsWith('Denied')) {
-            reject(new Error(title.split(/[ =]/)[2]));
-            closeWin();
-          } else if (title.startsWith('Success')) {
-            resolve(title.split(/[ =]/)[2]);
-            closeWin();
-          }
-        });
-      });
-    });
+    return this.openAuthPageAndGetAuthorizationCode(urlParam);
   }
 
+  async openAuthPageAndGetAuthorizationCode(urlParam: string) {
+    if (this.server) {
+      // if a server is already running, we close it so that we free the port
+      // and restart the process
+      await this.server.close();
+      this.server = null;
+    }
+    this.server = new LoopbackRedirectServer({
+      port: this.options.loopbackInterfaceRedirectionPort,
+      callbackPath: '/callback',
+      successRedirectURL: this.options.successRedirectURL,
+    });
+
+    shell.openExternal(urlParam);
+
+    const reachedCallbackURL = await this.server.waitForRedirection();
+
+    // waitForRedirection will close the server
+    this.server = null;
+
+    const parsed = url.parse(reachedCallbackURL, true);
+    if (parsed.query.error) {
+      throw new Error(parsed.query.error_description as string);
+    } else if (!parsed.query.code) {
+      throw new Error('Unknown');
+    }
+
+    if (this.options.refocusAfterSuccess) {
+      // refocus on the window
+      BW.getAllWindows().filter(w => w.isVisible()).forEach(w => w.show());
+    }
+    
+    return parsed.query.code as string
+  }
+  
   /**
    * Get Google tokens for given scopes
    * @param {boolean} forceAddSession
